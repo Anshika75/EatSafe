c5506d64f8430267b72bd31ecae4e4e58a223661
anshika75 <anshikathakur596@gmail.com>
2024-06-25 12:11:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home'
+import Nutrition from './components/nutrition'
 
 function App() {
 
   return (
     <>
       <div className="grid place-items-center w-full min-h-screen bg-[#F0EEEE]">
-        <Home />
+        <Nutrition/>
       </div>
     </>
   )
